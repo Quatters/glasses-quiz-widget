0ddfbea5451b2be0c185a4c681db7041d077fc3e
Quatters <vladnfs3@gmail.com>
2022-03-23 18:33:30 +1000

    Added timeout attribute to Between component

--- src/screens/Between.jsx
+++ src/screens/Between.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import { React, useEffect } from 'react';
 import like from '../img/like.svg';
 
 function Between(props) {
+  useEffect(() => {
+    if (props.timeout) {
+      setTimeout(() => {
+        props.onTimeout();
+      }, props.timeout);
+    }
+  }, [props]);
+
   return (
     <div className='d-flex flex-column align-items-center'>
       <div style={{ marginTop: '4rem' }}>
