4977793bb705e54a17c4c0cbfd59f25ccf76bbcc
Quatters <vladnfs3@gmail.com>
2022-03-23 18:32:53 +1000

    Added inline and additionalText attributes to Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,14 +4,24 @@ import '../style/Card.css';
 function Card(props) {
   return (
     <button
-      className='card d-flex flex-column align-items-center w-100'
-      style={{ padding: '2rem' }}
+      className={`card d-flex w-100 ${props.className} ${
+        props.inline
+          ? 'justify-content-between'
+          : 'flex-column align-items-center'
+      }`}
+      style={{
+        ...props.style,
+        padding: `${props.inline ? '1rem 1.5rem' : '2rem'}`,
+      }}
       onClick={props.onClick}
     >
       {props.img && (
         <img src={props.img} alt='' style={{ marginBottom: '1rem' }} />
       )}
       <p className='text-answer'>{props.text}</p>
+      {props.additionalText && (
+        <p className='text-answer-additional'>{props.additionalText}</p>
+      )}
     </button>
   );
 }
