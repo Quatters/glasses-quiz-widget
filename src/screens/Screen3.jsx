e9c9d727a9339e12b6f7c290fdfb3ae7896f6f28
Quatters <vladnfs3@gmail.com>
2022-03-23 18:34:09 +1000

    Refactored Screen3

--- src/screens/Screen3.jsx
+++ src/screens/Screen3.jsx
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from 'react';
+import { React, useState } from 'react';
 import Screen31 from './Screen31';
 import Between from './Between';
 import Screen32 from './Screen32';
@@ -7,28 +7,27 @@ function Screen3(props) {
   const [waiting, setWaiting] = useState(true);
   const [show32, setShow32] = useState(false);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setWaiting(false);
-    }, 2000);
-  }, []);
+  if (show32)
+    return (
+      <Screen32
+        onSkip={() => props.onNext()}
+        onEnd={parameter => props.onNext(null, parameter)}
+      />
+    );
+  else if (!waiting)
+    return (
+      <Screen31 onSkip={() => props.onNext()} onNext={() => setShow32(true)} />
+    );
 
-  function handleSkip() {
-    props.onNext();
-  }
-
-  function handleNext() {
-    setShow32(true);
-  }
-
-  function handleEnd(parameter) {
-    props.onNext(null, parameter);
-  }
-
-  if (waiting) return <Between text="Let's get to know you!" />;
-  else if (show32) return <Screen32 onSkip={handleSkip} onEnd={handleEnd} />;
-
-  return <Screen31 onSkip={handleSkip} onNext={handleNext} />;
+  return (
+    <Between
+      text="Let's get to know you!"
+      timeout={2000}
+      onTimeout={() => {
+        setWaiting(false);
+      }}
+    />
+  );
 }
 
 export default Screen3;
