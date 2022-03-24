a18ed2c9ebac7daf523758a964b79c79a82bc97a
Quatters <vladnfs3@gmail.com>
2022-03-24 20:44:50 +1000

    Screen5Eyeglasses fixed markup

--- src/screens/Screen5Eyeglasses.jsx
+++ src/screens/Screen5Eyeglasses.jsx
@@ -3,10 +3,7 @@ import Card from '../components/Card';
 
 function Screen5Eyeglasses(props) {
   return (
-    <div
-      className='d-flex flex-column align-items-center'
-      style={{ padding: '3rem' }}
-    >
+    <div className='screen'>
       <p className='text-question' style={{ marginBottom: '2.5rem' }}>
         Would you like to protect your eyes from light emanating from screens?
       </p>
