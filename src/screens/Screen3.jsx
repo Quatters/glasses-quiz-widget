88a4828d6480504b6e0e3399c70874f5d275c9ab
Quatters <vladnfs3@gmail.com>
2022-03-25 19:08:35 +1000

    Refactored Screen3 and Screen4

--- src/screens/Screen3.jsx
+++ src/screens/Screen3.jsx
@@ -7,39 +7,6 @@ import Screen32 from './Screen32';
 function Screen3(props) {
   const [showing, setShowing] = useState('between');
 
-  if (showing === '32')
-    return (
-      <SwitchTransition>
-        <CSSTransition
-          key={showing}
-          timeout={350}
-          classNames='screen'
-          unmountOnExit
-        >
-          <Screen32
-            onSkip={() => props.onNext()}
-            onEnd={parameter => props.onNext(null, parameter)}
-          />
-        </CSSTransition>
-      </SwitchTransition>
-    );
-  else if (showing === '31')
-    return (
-      <SwitchTransition>
-        <CSSTransition
-          key={showing}
-          timeout={350}
-          classNames='screen'
-          unmountOnExit
-        >
-          <Screen31
-            onSkip={() => props.onNext()}
-            onNext={() => setShowing('32')}
-          />
-        </CSSTransition>
-      </SwitchTransition>
-    );
-
   return (
     <SwitchTransition>
       <CSSTransition
@@ -48,13 +15,29 @@ function Screen3(props) {
         classNames='screen'
         unmountOnExit
       >
-        <Between
-          text="Let's get to know you!"
-          timeout={2000}
-          onTimeout={() => {
-            setShowing('31');
-          }}
-        />
+        <>
+          {showing === 'between' && (
+            <Between
+              text="Let's get to know you!"
+              timeout={2000}
+              onTimeout={() => {
+                setShowing('31');
+              }}
+            />
+          )}
+          {showing === '31' && (
+            <Screen31
+              onSkip={() => props.onNext()}
+              onNext={() => setShowing('32')}
+            />
+          )}
+          {showing === '32' && (
+            <Screen32
+              onSkip={() => props.onNext()}
+              onEnd={parameter => props.onNext(null, parameter)}
+            />
+          )}
+        </>
       </CSSTransition>
     </SwitchTransition>
   );
--- src/screens/Screen4.jsx
+++ src/screens/Screen4.jsx
@@ -7,41 +7,6 @@ import Screen42 from './Screen42';
 function Screen4(props) {
   const [showing, setShowing] = useState('41');
 
-  if (showing === 'between')
-    return (
-      <SwitchTransition>
-        <CSSTransition
-          key={showing}
-          timeout={350}
-          classNames='screen'
-          unmountOnExit
-        >
-          <Between
-            text="No worries, we've got you!"
-            timeout={2000}
-            onTimeout={() => {
-              setShowing('42');
-            }}
-          />
-        </CSSTransition>
-      </SwitchTransition>
-    );
-  else if (showing === '42')
-    return (
-      <SwitchTransition>
-        <CSSTransition
-          key={showing}
-          timeout={350}
-          classNames='screen'
-          unmountOnExit
-        >
-          <Screen42
-            onNext={parameter => props.onNext(null, parameter)}
-            onSkip={props.onNext}
-          />
-        </CSSTransition>
-      </SwitchTransition>
-    );
   return (
     <SwitchTransition>
       <CSSTransition
@@ -50,12 +15,31 @@ function Screen4(props) {
         classNames='screen'
         unmountOnExit
       >
-        <Screen41
-          onNext={parameter => props.onNext(null, parameter)}
-          onDontKnow={() => {
-            setShowing('between');
-          }}
-        />
+        <>
+          {showing === '41' && (
+            <Screen41
+              onNext={parameter => props.onNext(null, parameter)}
+              onDontKnow={() => {
+                setShowing('between');
+              }}
+            />
+          )}
+          {showing === 'between' && (
+            <Between
+              text="No worries, we've got you!"
+              timeout={2000}
+              onTimeout={() => {
+                setShowing('42');
+              }}
+            />
+          )}
+          {showing === '42' && (
+            <Screen42
+              onNext={parameter => props.onNext(null, parameter)}
+              onSkip={props.onNext}
+            />
+          )}
+        </>
       </CSSTransition>
     </SwitchTransition>
   );
